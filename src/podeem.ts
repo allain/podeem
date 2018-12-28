4496fb4242c45081fc05c91c5729ad16f1723111
Allain Lalonde <allain.lalonde@gmail.com>
2018-12-28 13:33:14 -0500

    working whittled down root

--- src/podeem.ts
+++ src/podeem.ts
@@ -1,82 +1,70 @@
-function createIterator(node: Node) {
-  const i = document.createNodeIterator(node, NodeFilter.SHOW_ELEMENT | NodeFilter.SHOW_TEXT)
-  return () => i.nextNode()
-}
-
-function extractRefName(node: Node): string | null {
-  if ('attributes' in node) {
-    let el: Element = node as Element
-    const attr = Array.from(el.attributes).find(a => a.name[0] === '#')
-    return attr ? el.removeAttributeNode(attr).name.slice(1) : null
-  }
-
-  let nodeData = node.nodeValue
-  if (!nodeData || nodeData[0] !== '#') {
-    return null
-  }
-
-  node.nodeValue = ''
-  return nodeData.slice(1)
-}
-
-type PathSegment = { skip: number, name: string }
 type RefMap = { [name: string]: Node }
+type Collectors = { [name: string]: number[] }
 
-function extractRefPath(node: Node): PathSegment[] {
-  const nextNode = createIterator(node)
-
-  let segments = []
-  let currentNode = nextNode()
-
-  let skip = 0
-  while (currentNode) {
-    const name = extractRefName(currentNode)
-    if (name) {
-      segments.push({ skip, name })
-      skip = -1
-    }
-    skip++
-    currentNode = nextNode()
+const childIndex = (node: Node, child: Node) => Array.from(node.childNodes).findIndex(n => n === child)
+const lineage = (descendant: Node, ancestor: Node): Node[] => {
+  const path = [descendant]
+  let currentNode: Node | null = descendant
+  while (currentNode && currentNode !== ancestor) {
+    currentNode = currentNode.parentNode
+    if (currentNode)
+      path.push(currentNode)
   }
-
-  return segments
+  return path
 }
 
-// Reused across all h calls
+const extractPath = (root: Node, descendant: Node) => {
+  const parents = lineage(descendant, root)
+  const path = []
+  for (let i = 0; i < parents.length - 1; i++) {
+    path.push(childIndex(parents[i + 1], parents[i]))
+  }
+  return path.reverse()
+}
+
+function extractCollectors(root: Node): { [name: string]: number[] } {
+  const iterator = document.createNodeIterator(root, NodeFilter.SHOW_ALL)
+
+  let collectors: Collectors = {}
+
+  let n: Node | null
+  while (n = iterator.nextNode()) {
+    if (n.nodeType === Node.TEXT_NODE && n.nodeValue && n.nodeValue[0] === '#') {
+      collectors[n.nodeValue.substr(1)] = extractPath(root, n)
+      n.nodeValue = ''
+    } else if (n.nodeType === Node.ELEMENT_NODE) {
+      const el = n as Element
+      const attr = Array.from(el.attributes).find((attr: Attr) => attr.name[0] === '#')
+      if (attr) {
+        collectors[attr.name.substr(1)] = extractPath(root, n)
+        el.removeAttributeNode(attr)
+      }
+    }
+  }
+
+  return collectors
+}
+
+const collector = (collectors: Collectors) =>
+  (node: Node): RefMap =>
+    Object.keys(collectors).reduce((result, name) => ({
+      ...result, [name]:
+        collectors[name].reduce((currentNode, index) => currentNode.childNodes.item(index), node)
+    }), {})
+
 const compilerTemplate = document.createElement('template')
 
 export default function h(strings: TemplateStringsArray, ...args: any[]) {
   const template = String.raw(strings, ...args)
-    .replace(/>\n+/g, '>')
     .replace(/\s+</g, '<')
     .replace(/>\s+/g, '>')
     .replace(/\n\s+/g, '<!-- -->')
 
   compilerTemplate.innerHTML = template
-  const content: Node | null = compilerTemplate.content.firstChild
-  if (!content) {
-    throw new Error('invalid template')
-  }
-
-  const refPath = extractRefPath(content)
+  const content = compilerTemplate.content.firstChild as Node
 
   const result = () => content.cloneNode(true)
 
-  result.collect = function collect(node: Node) {
-    const next = createIterator(node)
-
-    const refs: RefMap = {}
-
-    for (let { skip, name } of refPath) {
-      while (skip-- > 0) next()
-
-      const el = next()
-      if (el !== null) {
-        refs[name] = el
-      }
-    }
-
-    return refs
-  }
+  result.collect = collector(extractCollectors(content))
   return result
 }
--- test/podeem.test.ts
+++ test/podeem.test.ts
@@ -16,35 +16,31 @@ describe("podeem", () => {
     expect(builder()).not.toBe(builder())
   })
 
-  it('throws on invalid template', () =>
-    expect(() => h``).toThrowError('invalid template')
-  )
-
   it('builder exposes collect method', () => {
     const builder = h`<h1 #arg>#body</h1>`
     expect(builder.collect).toBeInstanceOf(Function)
   })
 
   it('collect method collects references', () => {
-    const builder = h`<h1 #arg>#body</h1>`
-    const built = builder()
-    const refs = builder.collect(built)
+    const build = h`<h1 #arg>#body</h1>`
+    const built = build()
+    const refs = build.collect(built)
     expect(Object.keys(refs)).toEqual(['arg', 'body'])
     expect(refs.arg.nodeName).toEqual('H1')
     expect(refs.body.nodeName).toEqual('#text')
   })
 
   it('handles collect without references', () => {
-    const builder = h`<h1>Static</h1>`
-    const built = builder()
-    expect(builder.collect(built)).toEqual({})
+    const build = h`<h1>Static</h1>`
+    const built = build()
+    expect(build.collect(built)).toEqual({})
   })
 
   it('collect can be used with any clones', () => {
-    const builder = h`<h1 #a>Static</h1>`
+    const build = h`<h1 #a>Static</h1>`
 
-    const builds = [builder(), builder(), builder()]
-    const collectedRefKeys = builds.map(builder.collect).map(Object.keys)
+    const builds = [build(), build(), build()]
+    const collectedRefKeys = builds.map(build.collect).map(Object.keys)
     expect(collectedRefKeys).toEqual([['a'], ['a'], ['a']])
   })
 })
